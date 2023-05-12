37f025f91862dd93a21f35d7f51a203df1c5863c
AbduTchop <115070603+AbduTchop@users.noreply.github.com>
2023-05-12 17:20:23 +0200

    homepage

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,30 @@
 const setup = () => {
 
+    let welcomeKey;
+    const welcome = () => {
+        let welcome = new Audio('audio/welcome.mp3');
+        welcome.autoplay = true;
+        welcome.play().then(r => r);
+        localStorage.setItem("visited", "true");
+    }
+
+    const restoreWelcome = () => {
+        welcomeKey = JSON.parse(localStorage.getItem("visited"));
+    }
+
+    const checkWelcome = () => {
+        welcomeKey = localStorage.getItem("visited") !== null;
+    }
+
+    const main = () => {
+        checkWelcome();
+        if (welcomeKey===false) {
+            welcome()
+        }
+    }
+
+    restoreWelcome();
+    main();
 }
 
 window.addEventListener("load", setup);
